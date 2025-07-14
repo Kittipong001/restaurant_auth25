8889c541201668ae800a62fd46aeea2df76b425c
Kittipong001 <xnokewro@gmail.com>
2025-07-14 14:41:27 +0700

    New Branch

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Restaurant Restful API");
