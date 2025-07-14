5acbc8e683369d891d4794e814d2a0c08f19b030
Kittipong001 <xnokewro@gmail.com>
2025-07-14 17:23:56 +0700

    C completed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
+import restaurantRouter from "./routers/restaurant.router.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,8 @@ app.get("/", (req, res) => {
   res.send("Restaurant Restful API");
 });
 
+//use router
+app.use("/api/v1/restaurants", restaurantRouter);
 app.listen(PORT, () => {
   console.log("Listening to http://localhost:" + PORT);
 });
